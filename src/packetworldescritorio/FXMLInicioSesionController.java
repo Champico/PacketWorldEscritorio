84d3a82ab6ad7f21e87b8a1fd532231632b0411f
Champico <champicoaps@gmail.com>
2025-11-29 10:45:17 -0600

    feat: Add Close button to login

--- src/packetworldescritorio/FXMLInicioSesionController.java
+++ src/packetworldescritorio/FXMLInicioSesionController.java
@@ -1,13 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**  @authores  Pipe, Kevin, champ */
+
 package packetworldescritorio;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.stage.Stage;
 
 
 public class FXMLInicioSesionController implements Initializable {
@@ -16,5 +17,14 @@ public class FXMLInicioSesionController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void clickIniciarSesion(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickCerrar(ActionEvent event) {
+        Platform.exit();
+    }
     
 }
--- src/packetworldescritorio/PacketWorldEscritorio.java
+++ src/packetworldescritorio/PacketWorldEscritorio.java
@@ -2,23 +2,34 @@
 
 package packetworldescritorio;
 
+import java.io.IOException;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import packetworldescritorio.utilidad.Utilidades;
 
 
 public class PacketWorldEscritorio extends Application {
     
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws Exception {
+       try{
+           Parent vista = FXMLLoader.load(getClass().getResource("FXMLInicioSesion.fxml"));
+           Scene escenaLogin = new Scene(vista);
+           primaryStage.setScene(escenaLogin);
+           primaryStage.initStyle(StageStyle.UNDECORATED);
+           primaryStage.setResizable(false);
+           primaryStage.show();
+       }catch(IOException ex){
+           Utilidades.mostrarAlertaSimple("Error al iniciar","Ocurrio un error al abrir la aplicación", Alert.AlertType.ERROR);
+           ex.printStackTrace();
+           Platform.exit();
+       }
     }
 
 
