d925f3ef075db9d3e46e0abcfb5b8737670a831b
Champico <champicoaps@gmail.com>
2025-12-19 14:13:08 -0600

    style: Add icon to login

--- src/packetworldescritorio/PacketWorldEscritorio.java
+++ src/packetworldescritorio/PacketWorldEscritorio.java
@@ -1,5 +1,4 @@
-/**  @authores  Pipe, Kevin, champ */
-
+/** @authores  Pipe, Kevin, champ */
 package packetworldescritorio;
 
 import java.io.IOException;
@@ -9,32 +8,37 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import packetworldescritorio.utilidad.Utilidades;
 
-
 public class PacketWorldEscritorio extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-       try{
-           Parent vista = FXMLLoader.load(getClass().getResource("FXMLInicioSesion.fxml"));
-           Scene escenaLogin = new Scene(vista);
-           primaryStage.setScene(escenaLogin);
-           primaryStage.initStyle(StageStyle.UNDECORATED);
-           primaryStage.setResizable(false);
-           primaryStage.show();
-       }catch(IOException ex){
-           Utilidades.mostrarAlertaSimple("Error al iniciar","Ocurrio un error al abrir la aplicación", Alert.AlertType.ERROR);
-           ex.printStackTrace();
-           Platform.exit();
-       }
-    }
+        try {
+            Parent vista = FXMLLoader.load(getClass().getResource("FXMLInicioSesion.fxml"));
+            Scene escenaLogin = new Scene(vista);
+            primaryStage.setScene(escenaLogin);
+            primaryStage.initStyle(StageStyle.UNDECORATED);
+            primaryStage.setResizable(false);
 
+            try {
+                primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/isotipo_low_definition.png")));
+            } catch (Exception ex) {
+            }
+
+            primaryStage.show();
+        } catch (IOException ex) {
+            Utilidades.mostrarAlertaSimple("Error al iniciar", "Ocurrio un error al abrir la aplicación", Alert.AlertType.ERROR);
+            ex.printStackTrace();
+            Platform.exit();
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
