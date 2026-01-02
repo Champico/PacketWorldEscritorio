350486cfe4aeb5096ddd5d1de9541591d31f1507
champicoaps-hash <champicoaps@gmail.com>
2026-01-02 11:00:18 -0600

    feat: create GUI of envio module

--- src/packetworldescritorio/FXMLModuloEnviosController.java
+++ src/packetworldescritorio/FXMLModuloEnviosController.java
@@ -3,16 +3,80 @@ package packetworldescritorio;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import packetworldescritorio.interfaz.INavegableChild;
+import packetworldescritorio.interfaz.INavegacion;
+import packetworldescritorio.pojo.Sucursal;
+import packetworldescritorio.utilidad.Constantes;
 
-public class FXMLModuloEnviosController implements Initializable {
+public class FXMLModuloEnviosController implements Initializable, INavegableChild {
 
-    /**
-     * Initializes the controller class.
-     */
+    private INavegacion nav;
+
+    @FXML
+    private TableView<?> tvEnvios;
+    @FXML
+    private TableColumn colGuia;
+    @FXML
+    private TableColumn colDestinatario;
+    @FXML
+    private TableColumn colCalle;
+    @FXML
+    private TableColumn colColonia;
+    @FXML
+    private TableColumn colCiudad;
+    @FXML
+    private TableColumn colEstado;
+    @FXML
+    private TableColumn colCodigoPostal;
+    @FXML
+    private TableColumn colEstatus;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+    }
+
+    @Override
+    public void setNavegador(INavegacion nav) {
+        this.nav = nav;
+    }
+
+    @Override
+    public void setObject(Object object) {
+
+    }
+
+    @FXML
+    private void clickIrRegistrar(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickIrBuscar(ActionEvent event) {
+    }
+
+    @FXML
+    private void clickRegresar(ActionEvent event) {
+        regresar();
+    }
+
+        private void regresar() {
+        nav.navegar(Constantes.PG_PRINCIPAL);
+    }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 }
